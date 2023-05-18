bf153412e18e266d252ad04783296c7898da72ee
Maisam Rizvi <maisamrizvi20@gmail.com>
2023-05-18 06:26:25 +0300

    Commit

--- tweet-it/app/page.tsx
+++ tweet-it/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1>Hello Next</h1>
+      <h1 className='text-lg py-5'>Hello Next</h1>
     </main>
   )
 }
